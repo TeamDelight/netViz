e892b22374eded68981d78fabff67211e5a09c03
marish <marishmadhav@gmail.com>
2020-10-04 10:54:03 +1100

    Db file as zip and updated gitignore

---
+++ netviz.py
@@ -0,0 +1,10 @@
+"""************************************************************************************************
+                                        Team Delight - NetViz
+Author: Team Delight
+Description: Module for app launch
+
+Modification History:
+Date        Changed by              Description
+04-10-2020  Marish_Varadaraj        Created a project layout and flask model for netviz project.
+************************************************************************************************"""
+from app import app
