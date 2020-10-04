773f1461047fe64345526b0b27468bf20a9034a3
Koushik Chakraborty <alarmbaba@gmail.com>
2020-10-04 21:30:06 +1100

    Adding structure as provided by Marish

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+"""************************************************************************************************
+                                        Team Delight - NetViz
+Author: Team Delight
+Description: Module for app launch
+
+Modification History:
+Date        Changed by              Description
+04-10-2020  Marish_Varadaraj        Created app configuration page.
+************************************************************************************************"""
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+from flask import Flask
+from config.web_config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
