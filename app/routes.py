6e36fd2db4f87f1b2034928680c343bb48927dad
marishmadhav <marishmadhav@gmail.com>
2020-10-18 08:40:07 +1100

    Ingegrating abd updating jakka code to match work in neteviz

--- app/routes.py
+++ app/routes.py
@@ -10,12 +10,14 @@ Date        Changed by              Description
 
 ************************************************************************************************"""
 
-from flask import Flask, flash,render_template, redirect, url_for, request
+import os
+from flask import Flask, flash,render_template, redirect, url_for, request, Response
 from app import app
 from app.main.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models.models import User
 from werkzeug.urls import url_parse
+import json
 
 
 """Initial login page for the netviz application
@@ -26,7 +28,7 @@ Returns:
 """
 @app.route('/')
 @app.route('/index')
-@login_required
+# @login_required
 def index():
     return render_template("index.html")
 
@@ -39,6 +41,9 @@ Returns:
     """
 @app.route('/login', methods=["GET","POST"])
 def login():
+    
+    return redirect(url_for('index'))
+
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     login_form = LoginForm()
@@ -66,3 +71,38 @@ Returns:
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/autocomplete',methods=['GET','POST'])
+def autocomplete():
+    #Have database Query to retrive customer names here
+    names_list=["Sam","Smile","Kumar","Kartik","Happy","Joy"]
+    return Response(json.dumps(names_list), mimetype='application/json')
+
+
+@app.route("/search",methods=['POST'])
+def search():
+    customer_search=request.form['autocomp']
+    #need to fetch customer data from the database
+    data_dict=[{"id":1,"name":"Kartik","phone":123,"Address":"Victoria"},
+            {"id":2,"name":"Kartik","phone":5768797,"Address":"Melbourne"}]
+    resulted_dict=[]
+    for p in data_dict:
+        if p['name'] == customer_search:
+            resulted_dict.append(p)
+    return render_template("index.html",resulted_dict=resulted_dict)
+
+
+@app.route("/graph_generation/<customer_id>")
+def graph_generation(customer_id):
+    file_location = os.getcwd().replace("\\","/") + "/graph_gen_sample.json"
+
+    return render_template("net_graph.html", data = jsonData(file_location))
+
+
+def jsonData(filePath):
+    with open(filePath) as graph_data:
+        data = json.load(graph_data)
+        return str(data)
+
+
